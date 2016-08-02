8dcdde5b264c31e47f41b59e267061f65b245a7e
Thayane Lopes <tl.1004@outlook.com>
2016-08-01 21:36:24 -0300

    Atualizando projeto

---
+++ elsez.php
@@ -0,0 +1,32 @@
+<?php 
+	$form = new PDO
+	("mysql:host=localhost;dbname=Elsez", "root","@luno1fpe");
+
+	try{
+
+		$x = $form->prepare("insert into usuario(nome,email,senha,nascimento, login) values (?,?,?,?,?)");
+
+		$nome = $_POST['nome'];
+		$email = $_POST['email'];
+		$senha = $_POST['senha'];
+		$nascimento = $_POST['nascimento'];
+		$login = $_POST['login'];
+
+		$x->bindParam(1,$nome);
+		$x->bindParam(2,$email);
+		$x->bindParam(3,$senha);
+		$x->bindParam(4,$nascimento);
+		$x->bindParam(5,$login);
+
+		$x->execute();
+		echo "Cadastro Realizado com sucesso!";
+
+	}catch(PDOException $e){
+	echo "Codigo do erro: ".$e->getCode()." - ".$e->getMessage();
+	}
+
+
+
+
+
+ ?>
