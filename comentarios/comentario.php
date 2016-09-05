b0aed1e04db9c8fbf8f91348973bb3d183f770cc
barbahamaria <barbaha.maria@gmail.com>
2016-09-04 22:48:19 -0300

    página de contato

--- comentarios/comentario.php
+++ comentarios/comentario.php
@@ -1,8 +1,9 @@
 <html>
 <head>
-<meta charset="utf-8">
-<title>Deixe seu Comentário!</title>
-<style type="text/css">
+	<meta charset="utf-8">
+	<title>Deixe seu Comentário!</title>
+	<link rel="stylesheet" href="../style.css">
+<!--<style type="text/css">
 	body{
 	background-image: url("images.png");
 	width: 100%;
@@ -19,23 +20,32 @@
 	border-color:#000;
 }
 
-</style>
+</style>-->
 </head>
 <body>
-<div class="com">
-	<form action="comentando.php" method="POST">
-		<table>
-			<tr>
-				<td><label>Nome:<label></td>
-				<td><input type="text" name="nome" placeholder="Insira seu nome"></td>
-			</tr>
-			<tr>
-				<td><label>Comentário:<label></td>
-				<td><textarea id="mensagem" name="mensagem" rows="10" cols="35" placeholder="Deixe aqui seu comentário sobre o fotografo ou fotos"></textarea></td>
-			</tr>
-		</table>
-		<input type="submit" value="Enviar"> <input type="reset" value="Limpar">
-	</form>
-</div>	
+	<div class="button">
+		<h1>
+			<a href="../index.html">Home</a>			
+		</h1>
+	</div>
+	<div class="com">
+		<form action="comentando.php" method="POST">
+			<input type="text" name="nome" placeholder="Insira seu nome"><br>
+			<center>
+					<textarea id="mensagem" name="mensagem" rows="10" cols="35" placeholder="Deixe aqui seu comentário sobre o fotografo ou fotos"></textarea>		
+			</center>
+			<!-- <table>
+				<tr>
+					<td><label>Nome:<label></td>
+					<td><input type="text" name="nome" placeholder="Insira seu nome"></td>
+				</tr>
+				<tr>
+					<td><label>Comentário:<label></td>
+					<td><textarea id="mensagem" name="mensagem" rows="10" cols="35" placeholder="Deixe aqui seu comentário sobre o fotografo ou fotos"></textarea></td>
+				</tr>
+			</table> -->
+			<input type="submit" value="Enviar"> <input type="reset" value="Limpar">
+		</form>
+	</div>	
 </body>
 </html>
